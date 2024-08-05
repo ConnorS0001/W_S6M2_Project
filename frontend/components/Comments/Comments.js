5abd00850fa34ae64107b2f4b4e647cd2b917a3d
Connor S <connmac0002@gmail.com>
2024-08-05 18:27:40 -0400

    finished

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -7,7 +7,9 @@ const Comments = props => {
 
   return (
     <div className='comments-wrapper'>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {/* map through the comments prop and render a Comment for every piece of data */
+      comments.map(com => <Comment comment={com} key={com.id} />)
+      }
     </div>
   );
 };
